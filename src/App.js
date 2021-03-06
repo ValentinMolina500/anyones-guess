4b463d0b64b687b76e58af20c58fcd8ebe1b5d13
Francisco Lopez <franktheballer23@gmail.com>
2021-03-06 12:34:59 -0800

    Updated main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Button, Grid, Heading, Input, Box } from '@chakra-ui/react';
+import { Button, Grid, Heading, Input, Box, background } from '@chakra-ui/react';
+import Background from './images/thinking.jpg';
+
+const styles = {
+  paperContainer: {
+    backgroundImage: `url(${Background})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+};
 
 
 function App() {
   return (
-    <Grid h="100vh" w="100vw" placeItems="center">
+    <Grid style={styles.paperContainer} h="100vh" w="100vw" placeItems="center">
       <Box textAlign="center" w="100%" maxW="20rem">
-        <Heading mb="2.5rem">Anyones Guess</Heading>
-        <Input mb="1rem" />
-        <Button w="100% ">Play Game</Button>
+        <Heading textColor="white" mb="1.5rem">Anyone's Guess</Heading>
+        <Input color="white" mb="1rem" placeholder="In Game Name"/>
+        <Button w="50%" >Play Game</Button>
       </Box>
     </Grid>
   );
