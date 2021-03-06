f563888a4ad4583416bb18e9657455115b23967f
Valentin Molina <valentinmolina83@gmail.com>
2021-03-06 11:36:53 -0800

    Update landing page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { Button, Grid, Heading, Input, Box } from '@chakra-ui/react';
 function App() {
   return (
     <Grid h="100vh" w="100vw" placeItems="center">
-      <Box textAlign="center">
-        <Heading mb="8">Anyones Guess</Heading>
-        <Input mb="8"></Input>
-        <Button>Play Game</Button>
+      <Box textAlign="center" w="100%" maxW="20rem">
+        <Heading mb="2.5rem">Anyones Guess</Heading>
+        <Input mb="1rem" />
+        <Button w="100% ">Play Game</Button>
       </Box>
     </Grid>
   );
