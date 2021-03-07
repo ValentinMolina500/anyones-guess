c5d3b185a6da0e9cca95a7a1e5072b7e9c073a37
Francisco Lopez <franktheballer23@gmail.com>
2021-03-06 20:55:32 -0800

    Added false input check functionality

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -14,9 +14,14 @@ const styles = {
 };
 
 function Landing() {
-  const handleChange = (event) => setValue(event.target.value)
-  const [value, setValue] = React.useState("")
 
+  let textEntered = false;
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  };
+
+  const [value, setValue] = React.useState("")
   const history = useHistory();
 
   const testAPI = async (username) =>{
@@ -30,8 +35,8 @@ function Landing() {
     <Grid style={styles.paperContainer} h="100vh" w="100vw" placeItems="center">
       <Box textAlign="center" w="100%" maxW="20rem">
         <Heading textColor="white" mb="1.5rem">Anyone's Guess</Heading>
-        <Input onChange={handleChange} color="white" mb="1rem" placeholder="In Game Name"/>
-        <Button w="50%" onClick={() => testAPI(value)}>Play Game</Button>
+        <Input  value={value} onChange={handleChange} color="white" mb="1rem" placeholder="In Game Name"/>
+        <Button disabled={!value} w="50%" onClick={() => testAPI(value)}>Play Game</Button>
       </Box>
     </Grid>
   )
